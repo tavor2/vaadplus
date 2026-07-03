620c2f465ee20ad79ac9351bd4d6c7db4544e2de
yaronzlot <yaron.zlotolov@gmail.com>
2026-07-03 14:13:36 +0300

    clean free

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('fetch', e => {
         }
         return response;
       }).catch(() => {
-        // Offline fallback — return main page
+        // Offline fallback — return main page1
         if (e.request.mode === 'navigate') {
           return caches.match('/vaadplus/index.html');
         }
