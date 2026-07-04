4b54b5b4f212e3bf2bdf4c43ac5f730a378046ad
yaronzlot <yaron.zlotolov@gmail.com>
2026-07-04 15:38:19 +0300

    bussines1

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const ASSETS = [
   '/vaadplus/icon-512.png'
 ];
 
-// Install — cache all assets
+// Install — cache all assets1
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
